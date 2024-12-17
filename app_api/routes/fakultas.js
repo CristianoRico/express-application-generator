bd59395260def1559f0934d8616841a0ed2c7724
CristianoRico <161105878+CristianoRico@users.noreply.github.com>
2024-12-17 16:58:13 +0700

    Update fakultas.js

--- app_api/routes/fakultas.js
+++ app_api/routes/fakultas.js
@@ -14,17 +14,17 @@ const roleMiddleware = require("../middleware/roleMiddleware");
 router.get("/", fakultasController.getAllFakultas);
 // Mengatur rute POST untuk membuat data fakultas baru
 router.post("/",
-    // authMiddleware,roleMiddleware("admin"),
+    authMiddleware,roleMiddleware("admin"),
     fakultasController.createFakultas);
 // Mengatur rute GET untuk mendapatkan data fakultas berdasarkan ID
 router.get("/:id", fakultasController.getFakultasById);
 // Mengatur rute PUT untuk memperbarui data fakultas berdasarkan ID
 router.put("/:id",
-    // authMiddleware,roleMiddleware("admin"),
+    authMiddleware,roleMiddleware("admin"),
     fakultasController.updateFakultas);
 // Mengatur rute DELETE untuk menghapus data fakultas berdasarkan ID
 router.delete("/:id",
-    // authMiddleware,roleMiddleware("admin"),
+    authMiddleware,roleMiddleware("admin"),
     fakultasController.deleteFakultas);
 
 // Mengeksport router agar dapat digunakan di file lain (misalnya, di app.js)
