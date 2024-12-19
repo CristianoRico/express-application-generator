b371a55099f4a13b567ceafef85af115b8af13c0
CristianoRico <161105878+CristianoRico@users.noreply.github.com>
2024-12-19 15:13:53 +0700

    Update mahasiswa.js

--- app_api/routes/mahasiswa.js
+++ app_api/routes/mahasiswa.js
@@ -14,13 +14,13 @@ router.get("/", mahasiswaController.getAllMahasiswa);
 router.get("/:id", mahasiswaController.getMahasiswaById);
 // Mengarahkan ke fungsi getMahasiswaById di controller untuk mengambil data mahasiswa berdasarkan ID
 // Route untuk menambahkan data mahasiswa baru, termasuk upload file foto
-router.post("/", upload.single("foto"), mahasiswaController.createMahasiswa);
+router.post("/", authMiddleware, roleMiddleware("admin"), upload.single("foto"), mahasiswaController.createMahasiswa);
 // Middleware `upload.single("foto")` menangani upload file dengan field `foto` sebelum fungsi createMahasiswa dijalankan
 // Route untuk memperbarui data mahasiswa, dengan upload foto opsional
-router.put("/:id", upload.single("foto"), mahasiswaController.updateMahasiswa);
+router.put("/:id", authMiddleware, roleMiddleware("admin"), upload.single("foto"), mahasiswaController.updateMahasiswa);
 // Middleware `upload.single("foto")` menangani upload file dengan field `foto` sebelum fungsi updateMahasiswa dijalankan
 // Route untuk menghapus data mahasiswa berdasarkan ID
-router.delete("/:id", mahasiswaController.deleteMahasiswa);
+router.delete("/:id", authMiddleware, roleMiddleware("admin"), mahasiswaController.deleteMahasiswa);
 // Mengarahkan ke fungsi deleteMahasiswa di controller untuk menghapus data mahasiswa berdasarkan ID
 
 module.exports = router; // Mengekspor router agar dapat digunakan di file lain
